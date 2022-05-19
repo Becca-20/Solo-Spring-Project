c322179b036f27f40db97a29ab1dafc16d0c77d9
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2022-05-19 10:50:38 +0100

    set up concert class + link to musician

--- src/main/java/com/example/music/model/Concert.java
+++ src/main/java/com/example/music/model/Concert.java
@@ -1,4 +1,85 @@
 package com.example.music.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "concerts")
 public class Concert {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private LocalDate localDate;
+
+    @Column
+    private int capacity;
+
+    @JsonIgnoreProperties({"concert"})
+    @OneToMany(mappedBy = "concert")
+    private List<Musician> musicians;
+
+
+    // no arg constructor
+    public Concert () {
+    }
+
+    // arg constructor
+    public Concert(String name, LocalDate localDate, int capacity) {
+        this.name = name;
+        this.localDate = localDate;
+        this.capacity = capacity;
+    }
+
+    // getters + setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public List<Musician> getMusicians() {
+        return musicians;
+    }
+
+    public void setMusicians(List<Musician> musicians) {
+        this.musicians = musicians;
+    }
 }
