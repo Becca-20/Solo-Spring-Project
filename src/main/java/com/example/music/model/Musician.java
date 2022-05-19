8f71a6966ab9a6b6d2a15718adc04cb3ddc2c186
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2022-05-19 10:43:15 +0100

    set up classes + repos, and basic musician class

---
+++ src/main/java/com/example/music/model/Musician.java
@@ -0,0 +1,78 @@
+package com.example.music.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "musicians")
+public class Musician {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private int number;
+
+    @Column
+    private String instrument;
+
+    @Column
+    private int grade;
+
+    // no arg constructor
+    public Musician() {
+    }
+
+    // arg constructor
+    public Musician (String name, int number, String instrument, int grade) {
+        this.name = name;
+        this.number = number;
+        this.instrument = instrument;
+        this.grade = grade;
+    }
+    
+    // getters + setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getInstrument() {
+        return instrument;
+    }
+
+    public void setInstrument(String instrument) {
+        this.instrument = instrument;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+}
