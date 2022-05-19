11d3d46425dae4d82c6816353eab6d9de3a13ee7
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2022-05-19 11:13:17 +0100

    set up repository interfaces

--- src/main/java/com/example/music/repository/ConcertRepository.java
+++ src/main/java/com/example/music/repository/ConcertRepository.java
@@ -1,4 +1,7 @@
 package com.example.music.repository;
 
-public interface ConcertRepository {
+import com.example.music.model.Concert;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConcertRepository extends JpaRepository<Concert, Long> {
 }
--- src/main/java/com/example/music/repository/MusicianRepository.java
+++ src/main/java/com/example/music/repository/MusicianRepository.java
@@ -1,4 +1,9 @@
 package com.example.music.repository;
 
-public interface MusicianRepository {
+import com.example.music.model.Musician;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MusicianRepository extends JpaRepository<Musician, Long> {
 }
