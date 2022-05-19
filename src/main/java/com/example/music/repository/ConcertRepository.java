0ef18df857898fb2a2772aa28e8416f56432f65f
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2022-05-19 15:34:27 +0100

    added data through h2 + tried out testing

--- src/main/java/com/example/music/repository/ConcertRepository.java
+++ src/main/java/com/example/music/repository/ConcertRepository.java
@@ -3,5 +3,11 @@ package com.example.music.repository;
 import com.example.music.model.Concert;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ConcertRepository extends JpaRepository<Concert, Long> {
+
+    // find all concerts with a capacity greater an 300
+    List<Concert> findByCapacityGreaterThan(int capacity);
+
 }
--- src/test/java/com/example/music/MusicApplicationTests.java
+++ src/test/java/com/example/music/MusicApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.music;
 
+import com.example.music.model.Concert;
+import com.example.music.repository.ConcertRepository;
+import com.example.music.repository.MusicianRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class MusicApplicationTests {
 
+	@Autowired
+	MusicianRepository musicianRepository;
+	ConcertRepository concertRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void canFindConcertCapacityOver300() {
+		List<Concert> found = concertRepository.findByCapacityGreaterThan(300);
+		assertThat(found.size()).isEqualTo(3);
 	}
 
 }
